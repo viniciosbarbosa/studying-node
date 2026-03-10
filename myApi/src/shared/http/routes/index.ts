27293a2896fd541681fecb20f4524f78ca92c66d
Vinicios Barbosa <viniciosbarbosagit@gmail.com>
2026-03-10 18:18:05 -0300

    criando middleware para error

--- myApi/src/shared/http/routes/index.ts
+++ myApi/src/shared/http/routes/index.ts
@@ -1,11 +1,10 @@
+import { AppError } from "@shared/erros/AppError";
 import { Router } from "express";
 
 const routes = Router();
 
 routes.get("/", (request, response) => {
-  return response.json({
-    message: "Hello World",
-  });
+  throw new AppError("Acesso negado");
 });
 
 export { routes };
--- myApi/src/shared/http/server.ts
+++ myApi/src/shared/http/server.ts
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import cors from "cors";
 import dotenv from "dotenv";
 import { routes } from "./routes";
+import { AppError } from "@shared/erros/AppError";
 
 dotenv.config();
 
@@ -11,6 +12,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  (
+    error: Error,
+    _request: Request,
+    response: Response,
+    _next: NextFunction,
+  ) => {
+    if (error instanceof AppError) {
+      const isInternalError = error.statusCode === 500;
+
+      return response.status(error.statusCode).json({
+        status: "error",
+        message: isInternalError ? "Internal server error" : error.message,
+      });
+    }
+    console.error("❌ Bug Inesperado:", error);
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  },
+);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started on port ${process.env.PORT || 3000}`);
