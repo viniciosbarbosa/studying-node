5bab0d1cb0b10682fc00552cced1fa5b32301c53
Vinicios Barbosa <viniciosbarbosagit@gmail.com>
2026-03-11 15:53:37 -0300

    create get roles

--- myApi/src/roles/http/routes/roles.routes.ts
+++ myApi/src/roles/http/routes/roles.routes.ts
@@ -12,4 +12,10 @@ rolesRouter.post("/", (request, response) => {
   return response.status(201).json(role);
 });
 
+rolesRouter.get("/", (request, response) => {
+  const roles = rolesRepository.findAll();
+
+  return response.json(roles);
+});
+
 export { rolesRouter };
